4ec7008d6728fbe6eed05c6824b8a43f4f57a369
dan-arki <100216647+dan-arki@users.noreply.github.com>
2023-11-16 12:08:17 +0100

    💄pages dones (country,game)

--- src/app/page.js
+++ src/app/page.js
@@ -1,34 +1,7 @@
 export default function Home() {
     return (
         <div class="bg-secondary text-white h-screen flex flex-col items-center justify-center gap-8">
-            <svg
-                width="97"
-                height="73"
-                viewBox="0 0 97 73"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-            >
-                <path
-                    d="M48.5 5L68 36.5L92.375 15.5L82.625 68H14.375L4.625 15.5L29 36.5L48.5 5Z"
-                    stroke="url(#paint0_linear_1_346)"
-                    stroke-width="9"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                />
-                <defs>
-                    <linearGradient
-                        id="paint0_linear_1_346"
-                        x1="-0.859375"
-                        y1="5"
-                        x2="63.3856"
-                        y2="90.9749"
-                        gradientUnits="userSpaceOnUse"
-                    >
-                        <stop stop-color="#000AFF" />
-                        <stop offset="1" stop-color="#A81AFF" />
-                    </linearGradient>
-                </defs>
-            </svg>
+            <img src="img/courrone.svg"></img>
             <div class="flex flex-col text-center">
                 <h1 class="text-3xl my-4 font-semibold">Votez et Découvrez!</h1>
                 <p class="text-base mb-6 text-light mx-2">
