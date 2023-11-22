d07b9be911579118a98414470144d06efbff982d
dan-arki <100216647+dan-arki@users.noreply.github.com>
2023-11-22 13:44:13 +0100

    page game added

--- stream-ranked/src/assets/components/game.jsx
+++ stream-ranked/src/assets/components/game.jsx
@@ -1,7 +1,13 @@
 export default function Game() {
     return (
-        <div>
-        <h2>Game</h2>
+        <div class="bg-secondary flex flex-col h-screen gap-16 items-center justify-center">
+            <h2>Ton jeu</h2>
+            <div class="flex flex-row gap-8">
+                <img src="img/fortnitelogo.jpg"></img>
+                <img src="img/logoMW3.jpg"></img>
+                <img src="img/valorant.jpg"></img>
+                <img src="img/apexLogo.jpg"></img>
+            </div>
         </div>
     );
 }
