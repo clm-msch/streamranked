4ea538629c8976db9221568e7c196325a4cb757f
dan-arki <100216647+dan-arki@users.noreply.github.com>
2023-11-22 13:43:00 +0100

    page welcome added

--- stream-ranked/src/assets/components/welcome.jsx
+++ stream-ranked/src/assets/components/welcome.jsx
@@ -1,7 +1,16 @@
-export default function Welcome() {
+export default function Home() {
     return (
-        <div>
-        <h2>Welcome</h2>
+        <div class="bg-secondary text-white h-screen flex flex-col items-center justify-center gap-8">
+            <img src="img/courrone.svg"></img>
+            <div class="flex flex-col text-center">
+                <h1 class="text-3xl my-4 font-semibold">Votez et Découvrez!</h1>
+                <p class="text-base mb-6 text-light mx-2">
+                    StreamRanked, votre plateforme ultime des streamers Twitch.
+                </p>
+            </div>
+            <button class=" text-3xl bg-gradient-to-r from-blue to-violet  text-white font-bold py-2 px-10 rounded hover:bg-purple-700">
+                Je vote
+            </button>
         </div>
     );
 }
